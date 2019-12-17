3dd023baf553a31946a693a2bd03f856f286f051
davbocle <david.bocle@misys.com>
2019-12-17 09:02:31 +0100

    fix hardcoded apiUrl

--- src/app/home/drive.service.ts
+++ src/app/home/drive.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DriveService {
-  api = 'http://localhost:3000/drive';
+  api = `${environment.AUTH_SERVER}/drive`;
 
   constructor(private http: HttpClient) {}
 
