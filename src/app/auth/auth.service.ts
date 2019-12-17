b600573d718ffe7bece9a5de5070ea0983ee4fe5
davbocle <david.bocle@misys.com>
2019-12-17 07:32:46 +0100

    use prod server for auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private AUTH_SERVER = 'http://localhost:3000';
   ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 
   constructor(private storage: Storage, private jwtHelper: JwtHelperService) {}
 
   login() {
-    window.location.href = `${this.AUTH_SERVER}/auth/google`;
+    window.location.href = `${environment.AUTH_SERVER}/auth/google`;
   }
 
   async setJwt(jwt: string) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  AUTH_SERVER: 'https://gsheets-db.herokuapp.com'
 };
