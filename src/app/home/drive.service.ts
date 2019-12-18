7a3564cabda9f4b9afba48302f1c9dc9009677a2
davbocle <david.bocle@misys.com>
2019-12-18 07:37:42 +0100

    no cache for /drive calls

--- src/app/home/drive.service.ts
+++ src/app/home/drive.service.ts
@@ -3,10 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-const headers = {
-  'ngsw-bypass': ''
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,22 +12,19 @@ export class DriveService {
   constructor(private http: HttpClient) {}
 
   getAppData(): Observable<ConfigFile> {
-    return this.http.get<ConfigFile>(`${this.api}/appdata`, { headers });
+    return this.http.get<ConfigFile>(`${this.api}/appdata`);
   }
 
   postAppData(location: string): Observable<ConfigFile> {
-    return this.http.post<ConfigFile>(`${this.api}/appdata`, {
-      location,
-      headers
-    });
+    return this.http.post<ConfigFile>(`${this.api}/appdata`, { location });
   }
 
   deleteAppData() {
-    return this.http.delete(`${this.api}/appdata`, { headers });
+    return this.http.delete(`${this.api}/appdata`);
   }
 
   getSheet(id: string, range = 'A1:C20') {
-    return this.http.get(`${this.api}/sheet/${id}?range=${range}`, { headers });
+    return this.http.get(`${this.api}/sheet/${id}?range=${range}`);
   }
 }
 
